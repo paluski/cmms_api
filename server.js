7365935dc1c6d985e86cfdb158ba3cf7c6144fd3
Eduardo Paluski <123786205+paluski@users.noreply.github.com>
2026-04-04 16:18:03 -0300

    Correção para web

--- server.js
+++ server.js
@@ -1504,7 +1504,7 @@ app.get("/relatorio/:id", auth, (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`API rodando na porta ${PORT}`);
 });
 
